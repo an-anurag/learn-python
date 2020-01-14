97e685987eed7eaa80f43b2609d28db379344398
capricorn <an.anurag@msn.com>
2020-01-14 20:55:55 +0530

    daily updated

--- programs/daily.py
+++ programs/daily.py
@@ -1,3 +1,4 @@
 """
 Convert string-hex to int, return integer if it can be convertible or else return passed string
+Create date regex with named group - (11,01/14/20,07:20:45,) - (?P<day>\d{2})\/(?P<month>\d{2})\/(?P<year>\d{2})[,\s+]?(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2})
 """
