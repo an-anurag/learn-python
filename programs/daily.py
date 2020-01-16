6745d834ed62d1bf8e46ee851a4a64ae0ade251f
appyens <an.anurag@msn.com>
2020-01-17 01:16:20 +0530

    added questions

--- programs/daily.py
+++ programs/daily.py
@@ -1,5 +1,14 @@
 """
+PYTHON
+
 Convert string-hex to int, return integer if it can be convertible or else return passed string
 Create date regex with named group - (11,01/14/20,07:20:45,) - re.compile("\(?\d{2}?,?\s?(?P<month>\d{2})\/(?P<day>\d{2})\/(?P<year>\d{2}),?\s?(?P<hour>\d{2}):(?P<minute>\d{2}):(?P<second>\d{2}),?\s?\)?")
 serialize python datetime object
+Update outer dictionary by adding two key value pairs from inner dictionary then delete the inner dictionary. Outer dictionary is in list. Again serialize the list with json
+
+AWS
+
+build alexa skill which calls external api endpoint for getting data to response user with voice on user input
+build alexa skill for notifying user about SIEM alarms, using alexa proactive events api
+create a lambda function deployment package with python and upload to lambda
 """
