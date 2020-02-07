65ca9869826ef910d8642297a6d20aa98bce1a55
capricorn <an.anurag@msn.com>
2020-02-07 20:28:58 +0530

    tasks updated

--- topics.py
+++ topics.py
@@ -1,12 +1,4 @@
 """
-Python:
-    never give built in python module names to custom modules
-    json.dumps data can be input to json.loads to make is valid json
-
-Django:
-    Overriding get_absolute_url method from settings
-    Adding fields to class using Model.add_to_class()
-    how to retrieve form_id in formset in django
 
 Web:
 How internet works
