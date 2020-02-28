a3f61d691000b4803f92ac742a4738c80437c105
appyens <an.anurag@msn.com>
2020-02-28 20:07:07 +0530

    tasks updated

--- tasks/linux_tasks.py
+++ tasks/linux_tasks.py
@@ -5,4 +5,7 @@ UBUNTU
 2. Count the items in directory --- ls -l | wc -l
 3. Filter files by extension --- grep
 4. Count files by extension --- ls -lR *.cfg | wc -l
+5. Create symlink
+6. setup crontab to automate script run
+7. Install deb package
 """
