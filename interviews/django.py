fc85b25909fcfc7498269cee3f74de6f7483dc50
appyens <an.anurag@msn.com>
2020-03-15 20:30:55 +0530

    notes updated

---
+++ interviews/django.py
@@ -0,0 +1,9 @@
+"""
+1. What is django model
+2. Have you any idea about django deployment
+3. Explain django OneToOne, OneToMany, ManyToMany fields
+4. Explain through kwarg in django
+5. what is makemigrations and migrate command
+6. what is makemessages command
+7. how will you handle longer running processes in views
+"""
