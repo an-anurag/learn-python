7f637e733e1494e47fb6a272c279014f97e927e4
Anurag Gundappa <41514107+appyens@users.noreply.github.com>
2021-05-09 00:00:29 +0530

    Update ubuntu_tasks.py

--- daily_tasks/ubuntu_tasks.py
+++ daily_tasks/ubuntu_tasks.py
@@ -8,4 +8,9 @@ UBUNTU
 5. Create symlink
 6. setup crontab to automate script run
 7. Install deb package
+8. update app packages
+9. extend the partitions (parted, fdisk)
+10. Mount the partition at boot (fstab)
+11. Configure allow/deny traffic firewall (ufw)
+12. Write shell script for common automation
 """
