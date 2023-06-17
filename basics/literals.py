6c8d5468cc9eeed4b3b1331289a83ecaf3731c28
Anurag Gundappa <41514107+an-anurag@users.noreply.github.com>
2023-06-17 17:52:47 +0530

    Create literals.py

---
+++ basics/literals.py
@@ -0,0 +1,61 @@
+# keywords
+
+import keyword
+thirty_five_words = keyword.kwlist
+print(thirty_five_words)
+
+# Literals
+
+# string
+my_name = "ABCD"
+print(my_name)
+
+your_name = 'ANURAG'
+print(your_name)
+
+multiline_string = """This is 
+multiline string, in python
+you can use this for multiline commenting 
+or doc strings"""
+print(multiline_string)
+
+multiline_string_with_single_quotes = '''This is 
+multiline string, in python
+you can use this for multiline commenting 
+or doc strings'''
+print(multiline_string_with_single_quotes)
+
+# numbers
+roll_number = 12345678
+ten = 0b1010
+sixty_three = 0o77
+hex_number = 0x11FF
+
+# float
+price = 123.44
+
+# complex
+complex_number = 3 + 10j
+
+# boolean 
+yes = True
+no = False
+
+# special literal
+nothing = None
+
+# list
+hobbies = ["singing", "dancing", "swimming"]
+print(hobbies)
+
+# tuple 
+my_skills = ("python", "ML", "data analysis")
+print(my_skills)
+
+# sets
+numbers = {1, 2, 3, 4, 5, 6, 7}
+print(numbers)
+
+# dictionary
+my_data = {"name": "Anurag", "age": 33, "hobby": "coding"}
+print(my_data)
